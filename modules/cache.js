1e8bb1b527a5cbfabf69715db533e523de37e208
Jose A Gonzalez <JGman13@gmail.com>
2022-10-02 16:16:27 -1000

    lab 10 done

--- modules/cache.js
+++ modules/cache.js
@@ -1,24 +1,3 @@
-class Cache
-{
+'use strict';
 
-	#cache = {
-		testMovie:
-			{
-				data: null, 
-				timeStamp: null 
-			}
-	};
-
-
-	searchCache(key)
-	{
-		return this.#cache[key] || false;
-	}
-
-	addToCache(key, data)
-	{
-		this.#cache[key] = data;
-	}
-}
-
-module.exports = Cache;
+module.exports = {};
--- modules/movie.js
+++ modules/movie.js
@@ -2,33 +2,42 @@
 
 const axios = require("axios");
 
-const cacheClass = require("./cache.js");
-
-const cache = new cacheClass();
+let cache = require('./cache.js');
 
 require('dotenv').config();
 
 class Movie {
-    constructor(title) {
-      this.title = title;
+  constructor(title) {
+    this.title = title;
+  }
+}
+
+async function handleMovies(req, res) {
+
+  try {
+    const { searchQuery } = req.query
+    const API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_DB_KEY}&query=${searchQuery}`;
+    const key = 'movies-' + searchQuery;
+
+    if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+      console.log('Cache hit');
+    } else {
+      console.log('Cache miss');
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
+      let movieData = await axios.get(API);
+      let movies = movieData.data.results.slice(0,4);
+      cache[key].data = movies.map(movieData => new Movie(movieData.title));
     }
+
+    res.send(cache[key].data);
   }
 
-  async function handleMovies(req,res){
 
-    try {
-      const { searchQuery} = req.query
-      const API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_DB_KEY}&query=${searchQuery}`;
-      console.log(API);
-      const movieResponse = await axios.get(API);
-      const movies = movieResponse.data.results.slice(0,4);
-      res.send(movies.map(movieData => new Movie(movieData.title)));
-    }
-  
-    catch (error) {
-      console.log(error);
-    }
-  };
-  
+  catch (error) {
+    console.log(error);
+  }
+}
 
-module.exports = handleMovies;
+
+  module.exports = handleMovies;
